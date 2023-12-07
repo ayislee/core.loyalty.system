9d510168da581659bdf1f48134571a7b5249244c
Ayi Restudiputra <ayislee001@gmail.com>
2023-12-07 11:44:00 +0700

    fix

--- app/Lib/LoyaltyLib.js
+++ app/Lib/LoyaltyLib.js
@@ -3,7 +3,8 @@ const UserPartner = use('App/Models/UserPartner')
 module.exports = {
     async getPartnerIDFromUser(userID) {
         try {
-            const up = await UserPartner.query().where('user_id',userID).first()    
+            const up = await UserPartner.query().where('user_id',userID).first()   
+            console.log(up) 
             if(up) {
                 return up.partner_id
             }else{
